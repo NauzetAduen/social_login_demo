018f73fa0f0a3d50bde2206f9655eb4cf4cbf486
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-10-05 20:47:07 +0100

    REMOVE file

--- test/services/authentication_service_test.dart
+++ test/services/authentication_service_test.dart
@@ -1,10 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mockito/mockito.dart';
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockGoogleSignIn extends Mock implements GoogleSignIn {}
+
+class MockUser extends Mock implements User {}
 
 void main() {
+  final MockFirebaseAuth mockFirebaseAuth = MockFirebaseAuth();
+  final MockGoogleSignIn mockGoogleSignIn = MockGoogleSignIn();
   setUp(() {});
   tearDown(() {});
 
-  test('', () async {
+  test('Sign in', () async {
     //
+    when(mockGoogleSignIn.signIn()).thenAnswer((_) async => null);
+    when(mockFirebaseAuth.currentUser).thenAnswer((_) => null);
+  });
+
+  test('Sign Out', () async {
+    //
+    when(mockFirebaseAuth.signOut()).thenAnswer((_) async => null);
+    when(mockFirebaseAuth.currentUser).thenAnswer((_) => null);
+
+    await mockFirebaseAuth.signOut();
+    expect(mockFirebaseAuth.currentUser, null);
   });
 }
